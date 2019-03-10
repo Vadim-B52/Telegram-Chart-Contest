512f247dd08b660869ae83f2587bbf1d009534d8
Vadim Belotitskiy <belotitskiy@devexperts.com>
2019-03-11 00:14:29 +0300

    writing client

--- Telegram Chart/Telegram Chart/Sources/Client/ChartClient.swift
+++ Telegram Chart/Telegram Chart/Sources/Client/ChartClient.swift
@@ -6,6 +6,7 @@
 import Foundation
 
 
+// TODO: add data check
 public final class ChartClient {
 
     public init() {
@@ -19,10 +20,83 @@ public final class ChartClient {
         guard let json = try? JSONSerialization.jsonObject(with: data) else {
             throw ChartClientError.noJson
         }
-        guard let remoteCharts =
+        guard let remoteCharts = json as? [Any] else {
+            throw ChartClientError.noChart
+        }
+        let charts = try remoteCharts.map { return try readChart($0) }
+        return ChartTransferObject(charts: charts)
+    }
+
+    private func readChart(_ remoteChart0: Any) throws -> ChartTransferObject.Chart {
+        guard let remoteChart = remoteChart0 as? [String: Any] else {
+            throw ChartClientError.noChart
+        }
+        let columns = try readColumns(remoteChart["columns"])
+        let columnKeys = columns.map{ $0.key }
+        let types = try readTypes(remoteChart["types"], columnKeys: columnKeys)
+        guard types.values.count > 1 else {
+            throw ChartClientError.noChart
+        }
+        let names = try readNames(remoteChart["names"])
+        let colors = try readColors(remoteChart["colors"])
+        return ChartTransferObject.Chart(columns: columns, types: types, names: names, colors: colors)
+    }
+
+    private func readColors(_ remoteColors: Any?) throws -> ChartTransferObject.Chart.Colors {
+        return try ChartTransferObject.Chart.Colors(values: readValues(remoteColors))
+    }
+
+    private func readNames(_ remoteNames: Any?) throws -> ChartTransferObject.Chart.Names {
+        return try ChartTransferObject.Chart.Names(values: readValues(remoteNames))
+    }
+
+    private func readTypes(_ remoteTypes0: Any?, columnKeys: [String]) throws -> ChartTransferObject.Chart.Types {
+        let remoteTypes = try readValues(remoteTypes0)
+        var types = [ChartTransferObject.Chart.ColumnKey: ChartTransferObject.Chart.ColumnType]()
+        for key in columnKeys {
+            guard let type = remoteTypes[key] else {
+                continue
+            }
+            switch type {
+            case ChartTransferObject.Chart.ColumnType.x.rawValue:
+                types[key] = .x
+            case ChartTransferObject.Chart.ColumnType.line.rawValue:
+                types[key] = .line
+            default:
+                continue
+            }
+        }
+        return ChartTransferObject.Chart.Types(values: types)
+    }
+
+    private func readValues(_ remotes0: Any?) throws -> [ChartTransferObject.Chart.ColumnKey: String] {
+        guard let remotes = remotes0 as? [String: String] else {
+            throw ChartClientError.noChart
+        }
+        return remotes
+    }
+
+    private func readColumns(_ remoteColumns0: Any?) throws -> [ChartTransferObject.Chart.Column] {
+        guard let remoteColumns = remoteColumns0 as? [[Any]] else {
+            throw ChartClientError.noChart
+        }
+        return try remoteColumns.map { try readColumn($0) }
+    }
+
+    private func readColumn(_ remoteColumn: [Any]) throws -> ChartTransferObject.Chart.Column {
+        guard let key = remoteColumn.first as? String else {
+            throw ChartClientError.noChart
+        }
+        var values = [Int64]()
+        for i in 1..<remoteColumn.count {
+            guard let val = remoteColumn[i] as? Int64 else {
+                throw ChartClientError.noChart
+            }
+            values.append(val)
+        }
+        return ChartTransferObject.Chart.Column(key: key, values: values)
     }
 }
-
 
 public enum ChartClientError: Error {
     case noData
--- Telegram Chart/Telegram Chart/Sources/DTO/ChartTransferObject.swift
+++ Telegram Chart/Telegram Chart/Sources/DTO/ChartTransferObject.swift
@@ -14,52 +14,58 @@ public class ChartTransferObject: NSObject {
     public init(charts: [Chart]?) {
         self.charts = charts
     }
-    
-    public class Chart: NSObject {
-        public let columns: [Column]?
-        public let types: Types?
-        public let names: Names?
-        public let colors: Colors?
 
-        public init(columns: [Column]?, types: Types?, names: Names?, colors: Colors?) {
+    // TODO: optionals ?
+    public class Chart: NSObject {
+        public let columns: [Column]
+        public let types: Types
+        public let names: Names
+        public let colors: Colors
+
+        public init(columns: [Column], types: Types, names: Names, colors: Colors) {
             self.columns = columns
             self.types = types
             self.names = names
             self.colors = colors
         }
 
-        public typealias ColumnType = String
+        public typealias ColumnKey = String
+
+        public enum ColumnType: String, RawRepresentable {
+            case x = "x"
+            case line = "line"
+        }
 
         public class Column: NSObject {
-            public let type: ColumnType?
-            public let values: [Int64]?
+            public let key: ColumnKey
+            public let values: [Int64]
 
-            public init(type: String?, values: [Int64]?) {
-                self.type = type
+            public init(key: String, values: [Int64]) {
+                self.key = key
                 self.values = values
             }
         }
         
         public class Types: NSObject {
-            public let values: [ColumnType: String]?
+            public let values: [ColumnKey: ColumnType]
 
-            public init(values: [ColumnType: String]?) {
+            public init(values: [ColumnKey: ColumnType]) {
                 self.values = values
             }
         }
         
         public class Names: NSObject {
-            public let values: [ColumnType: String]?
+            public let values: [ColumnKey: String]
 
-            public init(values: [ColumnType: String]?) {
+            public init(values: [ColumnKey: String]) {
                 self.values = values
             }
         }
         
         public class Colors: NSObject {
-            public let values: [ColumnType: String]?
+            public let values: [ColumnKey: String]
 
-            public init(values: [ColumnType: String]?) {
+            public init(values: [ColumnKey: String]) {
                 self.values = values
             }
         }
