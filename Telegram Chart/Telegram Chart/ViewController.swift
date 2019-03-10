20d5280e73954cdb8dcb65f73f84b168e6f85cc8
Vadim Belotitskiy <belotitskiy@devexperts.com>
2019-03-10 21:12:14 +0300

    Create Xcode project and worspace

---
+++ Telegram Chart/Telegram Chart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Telegram Chart
+//
+//  Created by Vadim on 10/03/2019.
+//  Copyright © 2019 Vadim Belotitskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
