a2b2f5712b1927de5156346c149fa44905f98e3a
Vadim Belotitskiy <belotitskiy@devexperts.com>
2019-04-14 13:40:25 +0300

    double y axis

--- Telegram Chart/Telegram Chart/Sources/UI/ChartViews/YAxisView.swift
+++ Telegram Chart/Telegram Chart/Sources/UI/ChartViews/YAxisView.swift
@@ -7,6 +7,8 @@ import UIKit
 
 class YAxisView: UIView, ChartViewProtocol {
 
+    private var views: [OneYAxis]
+
     private var chart: DrawingChart?
     var valuePanelConfig: ValueAxisPanel.Config! {
         didSet {
@@ -16,47 +18,161 @@ class YAxisView: UIView, ChartViewProtocol {
                         zeroAxisColor: .gray,
                         textColor: .gray)
             }
+            updateSkin()
         }
     }
 
     override init(frame: CGRect) {
+        var views = [OneYAxis]()
+        for _ in 0..<5 {
+            views.append(OneYAxis())
+        }
+        self.views = views
         super.init(frame: frame)
         contentMode = .redraw
         isOpaque = true
         backgroundColor = .clear
+        valuePanelConfig = nil
+
+        views.forEach { v in
+            addSubview(v)
+        }
     }
 
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        contentMode = .redraw
-        isOpaque = true
-        backgroundColor = .clear
+        fatalError("Not implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        guard let chart = chart else {
+            return
+        }
+
+        let plot = chart.visiblePlots.first!
+        let calculator = DrawingChart.YCalculator(valueRange: chart.valueRange(plot: plot))
+        let yAxisValues = chart.axisValues(plot: plot)
+
+        for (idx, view) in views.enumerated() {
+            let maxY = calculator.y(in: bounds, value: yAxisValues.zero + Int64(idx) * yAxisValues.step)
+            let height = view.sizeThatFits(.zero).height
+            view.frame = CGRect(x: bounds.minX, y: maxY - height, width: bounds.width, height: height)
+        }
     }
 
     func displayChart(_ chart:DrawingChart?, animation: ChartViewAnimation) {
         self.chart = chart
-        setNeedsDisplay()
+        updateSkin()
+        setNeedsLayout()
     }
 
     public override func draw(_ rect: CGRect) {
         super.draw(rect)
-        guard let chart = chart,
-              let ctx = UIGraphicsGetCurrentContext() else {
+        guard let ctx = UIGraphicsGetCurrentContext() else {
             return
         }
         let valuePanelConfig = self.valuePanelConfig!
         let (zeroLine, _) = bounds.divided(atDistance: ScreenHelper.lightLineWidth, from: .maxYEdge)
         valuePanelConfig.zeroAxisColor.setFill()
         ctx.fill(zeroLine)
+    }
 
-//        if let animationProgress = animationProgressDataSource?.animationProgressAlpha(chartView: self) {
-//            let color = valuePanelConfig.axisColor.withAlphaComponent(animationProgress)
-//            valuePanelConfig = ValueAxisPanel.Config(
-//                    axisColor: color,
-//                    zeroAxisColor: color,
-//                    textColor: valuePanelConfig.textColor.withAlphaComponent(animationProgress))
-//        }
-        let valuePanel = ValueAxisPanel(chart: chart, config: valuePanelConfig)
-        valuePanel.drawInContext(ctx, rect: bounds)
+    private func updateSkin() {
+        guard let chart = chart else {
+            return
+        }
+        for (idx, view) in views.enumerated() {
+            view.lineColor = valuePanelConfig.axisColor
+            var rightValue: NSMutableAttributedString!
+            for (plotIdx, plot) in chart.visiblePlots.enumerated() {
+                let firstPlotYAxisValues = chart.axisValues(plot: chart.visiblePlots.first!)
+                let firstPlotVal = firstPlotYAxisValues.zero + Int64(idx) * firstPlotYAxisValues.step
+                let yAxisValues = chart.axisValues(plot: plot)
+                let val = yAxisValues.zero + Int64(idx) * yAxisValues.step
+                if plotIdx == 0 {
+                    view.setLeftValue(NSAttributedString(
+                            string: "\(val)",
+                            attributes: [NSAttributedString.Key.foregroundColor: valuePanelConfig.textColor]))
+                } else if firstPlotVal != val {
+                    if rightValue == nil {
+                        rightValue = NSMutableAttributedString()
+                    }
+                    rightValue.append(NSAttributedString(
+                            string: ", ",
+                            attributes: [NSAttributedString.Key.foregroundColor: valuePanelConfig.textColor]))
+                    rightValue.append(NSAttributedString(
+                            string: "\(val)",
+                            attributes: [NSAttributedString.Key.foregroundColor: plot.color]))
+                }
+            }
+            if rightValue != nil {
+                rightValue.replaceCharacters(in: NSRange(location: 0, length: 2), with: "")
+            }
+            view.setRightValue(rightValue)
+//            let calculator = DrawingChart.YCalculator(valueRange: chart.valueRange(plot: plot))
+//            let maxY = calculator.y(in: bounds, value: yAxisValues.zero + Int64(idx) * yAxisValues.step)
+//            let height = view.sizeThatFits(.zero).height
+//            view.frame = CGRect(x: bounds.minX, y: maxY - height, width: bounds.width, height: height)
+        }
+    }
+}
+
+class OneYAxis: UIView {
+
+    private let leftLabel = UILabel()
+    private let rightLabel = UILabel()
+    private let font = Fonts.current.light11()
+
+    var lineColor: UIColor? {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    private func setup() {
+        contentMode = .redraw
+        isOpaque = true
+        backgroundColor = .clear
+        leftLabel.textAlignment = .left
+        rightLabel.textAlignment = .right
+
+        [leftLabel, rightLabel].forEach { label in
+            label.font = font
+            label.frame = bounds
+            label.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+            addSubview(label)
+        }
+    }
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        guard let ctx = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        let (line, _) = bounds.divided(atDistance: ScreenHelper.lightLineWidth, from: .maxYEdge)
+        lineColor?.setFill()
+        ctx.fill(line)
+    }
+
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        return CGSize(width: size.width, height: ceil(font.lineHeight))
+    }
+
+    func setLeftValue(_ value: NSAttributedString?) {
+        leftLabel.attributedText = value
+    }
+
+    func setRightValue(_ value: NSAttributedString?) {
+        rightLabel.attributedText = value
     }
 }
